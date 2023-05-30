3096af6a57948d2d40254ba4ca8e75b9b2a28d00
paolo <paolo.marrone@orastron.com>
2023-05-30 13:53:03 +0200

    license

--- src/vst3/config_vst3.h
+++ src/vst3/config_vst3.h
@@ -1,21 +1,21 @@
 /*
- * Brickworks
+ * Dynplug
  *
  * Copyright (C) 2022 Orastron Srl unipersonale
  *
- * Brickworks is free software: you can redistribute it and/or modify
+ * Copyright is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, version 3 of the License.
  *
- * Brickworks is distributed in the hope that it will be useful,
+ * Copyright is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Brickworks.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Copyright.  If not, see <http://www.gnu.org/licenses/>.
  *
- * File authors: Stefano D'Angelo, Paolo Marrone
+ * File authors: Paolo Marrone, Stefano D'Angelo
  */
 
 #ifndef _VST3_CONFIG_H
@@ -23,14 +23,14 @@
 
 #define PLUGIN_SUBCATEGORY	"Fx" // If Fx|Instrument Reaper (and only him) mixes input and output...
 
-#define PLUGIN_GUID_1		0x5af9b172
-#define PLUGIN_GUID_2		0x95ef439c
-#define PLUGIN_GUID_3		0xb10ed6f0
+#define PLUGIN_GUID_1		0x5af44172
+#define PLUGIN_GUID_2		0x95e1139c
+#define PLUGIN_GUID_3		0xb10aa6f0
 #define PLUGIN_GUID_4		0xb962eef1
 
-#define CTRL_GUID_1		0xed4990b0
-#define CTRL_GUID_2		0x89894215
-#define CTRL_GUID_3		0x96fc7cda
-#define CTRL_GUID_4		0x5a56cec9
+#define CTRL_GUID_1		0xed444400
+#define CTRL_GUID_2		0x898942a5
+#define CTRL_GUID_3		0x97fc7cda
+#define CTRL_GUID_4		0x5e56cec9
 
 #endif
